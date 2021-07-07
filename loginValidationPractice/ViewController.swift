86550c879b79addb5dc40ecae0d4d2d849b39562
SeoHyeokGyu <shk64099@gmail.com>
2021-07-07 13:59:54 +0900

    Initial Commit

---
+++ loginValidationPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  loginValidationPractice
+//
+//  Created by 서혁규 on 2021/07/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
